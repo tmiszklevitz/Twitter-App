8eb804581669e4b1066fca26c38cc35f5939de71
tmiszklevitz <troymiszklevitz@Troy-Miszklevitzs-Computer.local>
2016-12-19 00:53:26 -0600

    Added routing for Entertainment partial

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 //my scripts
 //<a class="twitter-timeline" href="https://twitter.com/search?q=celebrity" data-widget-id="809566881795424256">Tweets about celebrity</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+// <a class="twitter-timeline" href="https://twitter.com/search?q=Entertainment" data-widget-id="810733489435844608">Tweets about Entertainment</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 var app = angular.module("twitterApp", ["ngRoute","ngTweets"]);
 
 app.config(function($routeProvider) {
@@ -32,6 +33,11 @@ app.controller("celebCtrl", function ($scope, $http, tweets){
     // //     $scope.content = "Something went wrong";,,
     // });
 });
-app.controller("sportsCtrl", function ($scope, $http) {
-    //JSON data
+app.controller("sportsCtrl", function ($scope, $http, tweets){
+    //Load the JSON Data with an SHTTP request
+    tweets.get({
+        widgetId: '810733489435844608'
+    }).success(function(data){
+        $scope.feed = data;
+    });
 });
